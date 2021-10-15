ba8347528ab9068be4c22ff716b34a0742ba8d32
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2021-10-16 00:54:57 +0530

    Added Nav-Editor Component

---
+++ Components/Editor Bar/script.js
@@ -0,0 +1,19 @@
+const siteBtn = document.querySelector("#site-btn");
+
+function formatDoc(sCmd, sValue) {
+    document.execCommand(sCmd, false, sValue);
+}
+
+siteBtn.addEventListener("click", () => {
+    //Making Editable Content as false
+    var editable_elements = document.querySelectorAll("[contenteditable=true]");
+    for (var i = 0; i < editable_elements.length; i++)
+        editable_elements[i].setAttribute("contenteditable", false);
+    //Coding for the coding download link
+    const a = document.createElement("a")
+    const blob = new Blob(["<!doctype html><html><head><title>Print<\/title><\/head><body>" + container.innerHTML + "<\/body><\/html>"])
+    const dataUrl = URL.createObjectURL(blob)
+    a.href = dataUrl
+    a.download = "yourSite.html"
+    a.click()
+})
