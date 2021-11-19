918cd2b48a7bc5750eef9241de1bd243c6e6fee3
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2021-11-19 12:42:08 +0530

    Add custom preloader for the website.

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function menuToggle() {
     toggleMenu.classList.toggle('active');
     section.classList.toggle('active');
 }
+
+// function loaderFunc() {
+//     preloader.style.display = 'none';
+// }
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('loading');
+    preloader.classList.add('preload-finish')
+})
